acfa872d0f548002e252cf0e446acf4276d6b756
buihoangan <tantrum200@outlook.com>
2024-03-29 23:13:36 +0700

    bt5

---
+++ 3-process/bt5/main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<stdlib.h>
+
+int main(){
+    pid_t child_pid = -1;
+    printf("Hello, we are on process with pid: %d\n", getpid());
+    child_pid = fork();
+    // From here, we have two processes
+    if(child_pid == 0){
+        printf("****************\n");
+        printf("We are in child process with pid: %d\n", getpid());
+        printf("My parent process has pid: %d\n", getppid());
+        printf("****************\n");
+        while(1);
+    }
+    else{
+        printf("****************\n");
+        printf("We are in parent process with pid: %d\n", getpid());
+        printf("****************\n");
+        
+    }
+    printf("where am I, my pid %d\n", getpid());
+
+    return 0;
+}
